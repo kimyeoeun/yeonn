4c90b088c1174c9882755ae9ec2c643d87637c39
kimyeoeun <kdudms@naver.com>
2024-04-25 00:30:06 +0900

    Initial Commit

---
+++ detection_app_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  detection_app_2
+//
+//  Created by 김여은 on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ detection_app_2/detection_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  detection_app_2App.swift
+//  detection_app_2
+//
+//  Created by 김여은 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct detection_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
